dfde2a4a49122df6a9c9a9a457fc0695759025f8
Shubham18598 <muddamwarshubham98@gmail.com>
2022-07-20 20:00:23 +0530

    Homepage

---
+++ SM_file/index.js
@@ -0,0 +1,3 @@
+"https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"
+"https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+"https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"
